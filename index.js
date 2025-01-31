025b85873c1dde1c61b38c7d0a9cb31ca98bd726
timmy <sparynxcyberneti@gmail.com>
2025-01-31 15:38:26 +0100

    updated the cors to include the hosted domain name on vercel

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', "https://sparynxbudgetapp.vercel.app/"],
     credentials: true
 }));
 
